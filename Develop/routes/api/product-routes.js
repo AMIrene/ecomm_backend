a815dbd7e89a4ca1cf1c0c5bfbf628ae5aad665d
AMIrene <f.adammanik@gmail.com>
2021-08-22 16:50:38 +1000

    updated product code added to product route file

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -53,7 +53,7 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
+   /* req.body should look like this...
     {
       product_name: "Basketball",
       price: 200.00,
@@ -61,7 +61,15 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds
+  })
+ 
+  // Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -127,6 +135,12 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+    .then
+  })
 });
 
 module.exports = router;
